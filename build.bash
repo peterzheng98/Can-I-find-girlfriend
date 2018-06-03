84539263f906e92e9fe5b94b3cafba58cab677c1
peterzheng98 <33046323+peterzheng98@users.noreply.github.com>
2018-06-03 20:57:00 +0800

    13

--- build.bash
+++ build.bash
@@ -1,14 +1,3 @@
-mkdir tmp
-cd tmp
-rm -rf *
-touch userName.dat 
-touch train.dat 
-touch userTicket.dat 
-touch user.dat 
-touch ticketId.dat 
-touch ticket.dat 
-touch id.dat 
-cd ..
 g++ commandMain.cpp -o code -g -w -O2 -std=c++14
 ./code < FirstInit.lxqwe > oierhgiuwaesdgfbuweg.xldsfc
 
