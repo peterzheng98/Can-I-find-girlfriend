14accbbeb72499579ec7105a1a27f01324e528ff
peterzheng98 <33046323+peterzheng98@users.noreply.github.com>
2018-06-04 21:35:28 +0800

    25

--- build.bash
+++ build.bash
@@ -1,7 +1,3 @@
-echo '$0: '$0  
-echo "pwd: "`pwd`  
-echo "scriptPath1: "$(cd `dirname $0`; pwd)  
-echo "scriptPath2: "$(dirname $(readlink -f $0))  
 mkdir tmp
 cd tmp
 rm -rf *
@@ -15,6 +11,7 @@ touch id.dat
 cd ..
 ls -al
 clang --version
-g++ commandMain.cpp -o code -g -w -O1 -std=c++14
+g++ -v
+g++ commandMain.cpp -o code -w -O2 -std=c++14
 ./code < FirstInit.lxqwe > oierhgiuwaesdgfbuweg.xldsfc
 
